c105dd4d20cba4e2f594c38fea21ca30e63b7ac5
Aisha Ikram <aisha.ikram@gmail.com>
2022-09-05 17:54:42 +0100

    EventHub Producer and Consumer Consoles

---
+++ EventConsumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Storage.Blobs;
+
+internal class Program
+{
+
+
+    private const string connectionString = "<Event hub connection string>";
+    private const string eventHubName = "vacancyevents";
+
+    //access keys
+    private const string blobStorageConnectionString = "<blog connection string>";
+    private const string blobContainerName = "eventhubconsumer";
+    static async Task Main()
+    {
+        Console.WriteLine("Hello, From Consumer Group (aka application)");
+        string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+        // Create a blob container client that the event processor will use for checkpointing
+        BlobContainerClient storageClient = new BlobContainerClient(blobStorageConnectionString, blobContainerName);
+        // Create an event processor client to process events in the event hub
+        EventProcessorClient processor = new EventProcessorClient(storageClient, consumerGroup, connectionString, eventHubName);
+        // Register handlers for processing events and handling errors
+        processor.ProcessEventAsync += ProcessEventHandler;
+        processor.ProcessErrorAsync += ProcessErrorHandler;
+
+        // Start the processing
+        await processor.StartProcessingAsync();
+        // Wait for10 seconds for the events to be processed
+        await Task.Delay(TimeSpan.FromSeconds(10));
+        // Stop the processing
+        await processor.StopProcessingAsync();
+
+    }
+
+
+    static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+    {
+        // Write details about the error to the console window
+        Console.WriteLine($"\tPartition {eventArgs.PartitionId}' : an unhandled exception was encountered.");
+        Console.WriteLine(eventArgs.Exception.Message);
+        return Task.CompletedTask;
+
+    }
+
+
+    static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
+    {
+        // Write the body of the event to the console window
+
+        Console.WriteLine("\tReceived Event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+        // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+        await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
+    }
+}
---
+++ EventHubConsolePublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+internal class Program
+
+{
+
+    private const string connectionString = "Endpoint=sb://lgeventhub.servicebus.windows.net/;SharedAccessKeyName=Consumer;SharedAccessKey=h00BY83PM67RfZ0m6DkuQ3eZZU54k++UoimaMNySDCM=;EntityPath=vacancyevents";
+    private const string eventHubName = "vacancyevents";
+   
+
+
+    private static async Task Main()
+    {
+        Console.WriteLine("Hello, World!");
+        // Create a producer client that you can use to send events to an event hub
+        await using (var producerC1ient = new EventHubProducerClient(connectionString, eventHubName))
+        {
+            // Create a batch of events
+            using EventDataBatch eventBatch = await producerC1ient.CreateBatchAsync();
+            // Add events to the batch. An event is a represented by a collection of bytes and metadata.
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("First event")));
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Second event")));
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes("Third event")));
+
+            // Use the producer client to send the batch of events to the event hub
+            await producerC1ient.SendAsync(eventBatch);
+            Console.WriteLine("A batch of 3 events has been published. ");
+        }
+    }
+
+}
+
+
